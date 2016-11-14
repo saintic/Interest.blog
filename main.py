74a84e5c1774ef6f5535bcc7120c7398a524de94
Mr.tao <staugur@vip.qq.com>
2016-11-14 19:06:01 +0800

    admin user.html

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def before_request():
     g.expires   = request.cookies.get("time", "")
     g.signin    = isLogged_in('.'.join([ g.username, g.expires, g.sessionId ]))
     logger.info("Start Once Access, and this requestId is %s, isLogged_in:%s" %(g.requestId, g.signin))
+    app.logger.debug(app.url_map)
 
 #Each return data in response to head belt, including the version and the requestId access log records request.
 @app.after_request
--- views/admin.py
+++ views/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf8 -*-
 
+import requests
 from flask import Blueprint, render_template
+from utils.public import chunks
 
 admin_page = Blueprint("admin", __name__)
 
@@ -8,3 +10,13 @@ admin_page = Blueprint("admin", __name__)
 def AdminIndex():
     return render_template("admin/index.html")
 
+@admin_page.route("/user/")
+def AdminUser():
+    url  = "https://api.saintic.com/user?getalluser=true"
+    data = requests.get(url, timeout=5, verify=False, headers={"User-Agent": "staugur/Interest.blog"}).json().get("data", [])
+    data = chunks(data, 5)
+    return render_template("admin/user.html", data=data)
+
+@admin_page.route("/blog/")
+def AdminBlog():
+    return render_template("admin/blog.html")
