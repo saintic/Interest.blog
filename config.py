83fc1ac7a258b5d9efdec9b5066161b82f2f1c5b
Mr.tao <staugur@vip.qq.com>
2016-11-24 01:22:28 +0800

    update config.py for os.getenv

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 
 GLOBAL={
 
-    "Host": os.environ.get("interest.blog.host", "0.0.0.0"),
+    "Host": os.environ.get("interest.blog.Host", "0.0.0.0"),
     #Application run network address, you can set it `0.0.0.0`, `127.0.0.1`, or someone IP
 
-    "Port": int(os.environ.get("interest.blog.port", 10140)),
+    "Port": int(os.environ.get("interest.blog.Port", 10140)),
     #Application run port, default port
 
-    "LogLevel": os.environ.get("interest.blog.loglevel", "DEBUG"),
+    "LogLevel": os.environ.get("interest.blog.LogLevel", "DEBUG"),
     #Write log's level, current is DEBUG，INFO，WARNING，ERROR，CRITICAL
 }
 
@@ -19,7 +19,7 @@ PRODUCT={
     "ProcessName": "Interest.blog",
     #Custom process, you can see it with "ps aux|grep ProcessName"(with setproctitle module)
 
-    "ProductType": os.environ.get("interest.blog.producttype", "tornado"),
+    "ProductType": os.environ.get("interest.blog.ProductType", "tornado"),
     #Production environment starting method, optional: `gevent`, `tornado`
 }
 
@@ -34,33 +34,25 @@ SSO={
 
 PLUGINS={
 
-    "CodeHighlighting": True,
+    "CodeHighlighting": os.environ.get("interest.blog.CodeHighlighting", True),
 
-    "BaiduAutoPush": True,
+    "BaiduAutoPush": os.environ.get("interest.blog.BaiduAutoPush", True),
 
-    "DuoshuoComment": True,
+    "DuoshuoComment": os.environ.get("interest.blog.DuoshuoComment", True),
 
-    "Weather": True,
+    "Weather": os.environ.get("interest.blog.Weather", True),
 
-    "RealProbability": False,
+    "RealProbability": os.environ.get("interest.blog.RealProbability", False),
 
-    "BaiduStatistics": True,
+    "BaiduStatistics": os.environ.get("interest.blog.BaiduStatistics", True),
 
-    "BaiduShare": True,
+    "BaiduShare": os.environ.get("interest.blog.BaiduShare", True),
 }
 
 BLOG={
 
-    "AdminPrefix": "/admin",
+    "AdminPrefix": os.environ.get("interest.blog.AdminPrefix", "/admin"),
 }
 
-MYSQL={
-    "Host": "127.0.0.1",
-    "Port": 3306,
-    "Database": "saintic",
-    "User": "root",
-    "Passwd": "123456",
-    "Charset": "utf8",
-    "Timezone": "+8:00",
-    #MySQL连接信息，格式可包括在()、[]、{}内，分别填写主机名或IP、端口、数据库、用户、密码、字符集、时区等，其中port默认3306、字符集默认utf8、时区默认东八区，注意必须写在一行内！
-}
+MYSQL="mysql://host:port:user:password:database?charset=utf8&timezone=+8:00"
+#Format is mysql://host:port:user:password:database?charset=&timezone=
