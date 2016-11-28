2497423db7cad5860a9c55b33eb7a6693c4627c6
Mr.tao <staugur@vip.qq.com>
2016-11-28 17:26:58 +0800

    update feed

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def sitemap():
 @app.route("/feed/")
 def feed():
     data = requests.get(g.apiurl + "/blog", timeout=5, verify=False, headers={'User-Agent': 'Interest.blog/%s' %__version__}, params={"sort": "desc", "limit": "10"}).json().get("data") or []
-    feed = AtomFeed('The Latest Ten Articles', feed_url=request.url, url=request.url_root, subtitle="Interest.blog Feed(Atom)")
+    feed = AtomFeed('Interest.blog Latest Feed(Atom)', feed_url=request.url, url=request.url_root)
     for article in data:
         feed.add(article['title'], unicode(article['content']),
                  content_type='html',
