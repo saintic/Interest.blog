0004490d14980d659f48d397f5e412c93cd97ee7
Mr.tao <staugur@vip.qq.com>
2016-12-12 21:16:36 +0800

    修复api获取评论接口数据的bug

--- views/api.py
+++ views/api.py
@@ -8,10 +8,14 @@ api_page = Blueprint("api", __name__)
 
 @api_page.route("/comments/")
 def ApiComments():
-    res  = {"code": 0, "data": None, "msg": None}
+    res  = {"code": 0, "data": [], "msg": None}
     args = dict(short_name=g.plugins['DuoshuoComment']['shortName'], range="all", num_items=request.args.get("limit", 10))
-    data = requests.get("http://api.duoshuo.com/sites/listTopThreads.json", params=args, timeout=5, headers={"User-Agent": "Interest.blog/www.saintic.com"}).json()
-    blog = [ _ for _ in data.get("response") if _.get("comments") != 0 ]
-    res.update(data=blog, code=data["code"])
+    try:
+        data = requests.get("http://api.duoshuo.com/sites/listTopThreads.json", params=args, timeout=10, headers={"User-Agent": "Interest.blog/www.saintic.com"}).json()
+        blog = [ _ for _ in data.get("response") if _.get("comments") != 0 ]
+    except Exception,e:
+        logger.error(e, exc_info=True)
+    else:
+        res.update(data=blog, code=data["code"])
     logger.info(res)
     return jsonify(res)
